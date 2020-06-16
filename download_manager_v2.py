d2f40113440644cac4e609445769538b001cf922
Stephen Chen <chendeyao000@gmail.com>
2020-06-17 02:09:39 +0800

    added progress bar

--- download_manager_v2.py
+++ download_manager_v2.py
@@ -1,11 +1,12 @@
 import requests
 from os import path
 import boto3
+import sys
+import progressbar
 if __name__ == '__main__':
     with open('archive_links.txt') as links_f:
         for url in links_f:
             url = url.strip()
-            url = 'https://file-examples.com/wp-content/uploads/2019/09/file-sample_300kB.rtf'
             if not url:
                 continue
             f_name = url.rsplit('/', 1)[-1]
@@ -15,17 +16,20 @@ if __name__ == '__main__':
             # below is downloading content
 
             with open(f_path, 'wb') as f:
-                response = requests.get(url, allow_redirects=True)
+                response = requests.get(url, allow_redirects=True, stream=True)
                 total_length = response.headers.get('content-length')
 
                 if total_length is None:  # no content length header
                     f.write(response.content)
                 else:
-                    dl = 0
                     total_length = int(total_length)
-                    for data in response.iter_content(chunk_size=4096):
-                        dl += len(data)
-                        f.write(data)
-                        done = int(50 * dl / total_length)
-                        sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
-                        sys.stdout.flush()
+                    with progressbar.ProgressBar(max_value=total_length) as bar:
+                        dl = 0
+                        for data in response.iter_content(chunk_size=4096):
+                            dl += len(data)
+                            f.write(data)
+                            # noinspection PyBroadException
+                            try:
+                                bar.update(dl)
+                            except:
+                                pass

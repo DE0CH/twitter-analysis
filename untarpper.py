7321af3d7151f8744038fef5a17501ede436c0e6
DE0CH <chendeyao000@gmail.com>
2020-06-19 15:18:15 +0800

    update untarpper so it logs errors

--- untarpper.py
+++ untarpper.py
@@ -1,7 +1,12 @@
-import subprocess
-import pickle
+import coloredlogs
+import logging
 import os
+import pickle
+import subprocess
+
 if __name__ == '__main__':
+    coloredlogs.install()
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
     processes = []
     try:
         with open('untarred.pkl', 'rb') as f:
@@ -16,11 +21,29 @@ if __name__ == '__main__':
                 continue
             file_name_no_extension, file_extension = os.path.splitext(file)
             os.makedirs(os.path.join('untarred', file_name_no_extension), exist_ok=True)
-            processes.append(subprocess.Popen(
-                ['tar', '-xf', os.path.join(path, file), '-C', os.path.join('untarred', file_name_no_extension)]))
+            processes.append(
+                (
+                    file_path,
+                    subprocess.Popen(
+                        [
+                            'tar',
+                            '-xf', os.path.join(path, file),
+                            '-C', os.path.join('untarred', file_name_no_extension)
+                        ],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE
+                    )
+                )
+            )
             untarred.add(file_path)
-    for process in processes:
+    for file_path, process in processes:
         process.wait()
+        out, err = process.communicate()
+        if out:
+            logging.info(f'message: {file_path} -- {out}')
+        if err:
+            logging.error(f'failed: {file_path} -- {err}')
+
     with open('untarred.pkl', 'wb') as f:
         pickle.dump(untarred, f)
 

04ac0ff847756e6a191ae72c07fcd9076e4f67db
DE0CH <chendeyao000@gmail.com>
2020-06-22 01:02:57 +0800

    updated goe_filter so it doesn't use google trans

--- geo_filter.py
+++ geo_filter.py
@@ -53,8 +53,10 @@ if __name__ == '__main__':
         os.makedirs('processed')
     except FileExistsError:
         pass
+    processes = []
     for i in range(100):
         p = multiprocessing.Process(target=worker, args=(q, geo_filtered_dict))
+        processes.append(p)
         p.start()
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'twitter-sentiment-analysis-f22ce784b0a8.json'
     try:
@@ -67,5 +69,7 @@ if __name__ == '__main__':
         with open('geo_filtered.pkl', 'wb') as f:
             pickle.dump(geo_filtered, f)
         print('done')
+        for p in processes:
+            p.kill()
 
 

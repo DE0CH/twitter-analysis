5533e58d9b0d5eedacf270cddeb809337387b85d
DE0CH <chendeyao000@gmail.com>
2020-06-22 11:42:24 +0800

    fix bug in geofilter that it would record processed files

--- geo_filter.py
+++ geo_filter.py
@@ -9,35 +9,27 @@ import pickle
 
 def worker(q, geo_filtered_dict):
     while True:
-        path, dirs, files = q.get()
-        process_files(path, dirs, files, geo_filtered_dict)
+        path, file_name = q.get()
+        process_files(path, file_name, geo_filtered_dict)
         q.task_done()
 
 
-def process_files(path, dirs, files, geo_filtered_dict):
-    for file_name in files:
-        if os.path.join(path, file_name) in geo_filtered_dict:
-            continue
-        file_name_no_extension, file_extension = os.path.splitext(file_name)
-        out_file_name = os.path.join('processed', os.path.relpath(path, start='untarred'), file_name_no_extension)
-        os.makedirs(os.path.dirname(out_file_name), exist_ok=True)
-        with open(out_file_name, 'w') as out_file:
-            if file_name.endswith('.json.bz2'):
-                try:
-                    with open(os.path.join(path, file_name), 'rb') as file:
-                        decompressed_string = bz2.decompress(file.read())
-                        lines = decompressed_string.decode('utf-8').split('\n')
-                        for line in lines:
-                            if not line.strip():
-                                continue
-                            tweet = json.loads(line)
-                            if tweet.get('place', None) is not None and tweet['place']['country_code']:
-                                out_file.write(line)
-                    logging.info('finished ' + os.path.join(path, file_name))
-                except KeyboardInterrupt as e:
-                    os.remove(os.path.join(path, file_name))
-                    raise e
-        geo_filtered_dict[os.path.join(path, file_name)] = True
+def process_files(path, file_name, geo_filtered_dict):
+    file_name_no_extension, file_extension = os.path.splitext(file_name)
+    out_file_name = os.path.join('processed', os.path.relpath(path, start='untarred'), file_name_no_extension)
+    os.makedirs(os.path.dirname(out_file_name), exist_ok=True)
+    with open(out_file_name, 'w') as out_file:
+        with open(os.path.join(path, file_name), 'rb') as file:
+            decompressed_string = bz2.decompress(file.read())
+            lines = decompressed_string.decode('utf-8').split('\n')
+            for line in lines:
+                if not line.strip():
+                    continue
+                tweet = json.loads(line)
+                if tweet.get('place', None) is not None and tweet['place']['country_code']:
+                    out_file.write(line)
+        logging.info('finished ' + os.path.join(path, file_name))
+    geo_filtered_dict[os.path.join(path, file_name)] = True
 
 
 if __name__ == '__main__':
@@ -58,24 +50,19 @@ if __name__ == '__main__':
     except FileExistsError:
         pass
     processes = []
-    for i in range(100):
+    for i in range(20):
         p = multiprocessing.Process(target=worker, args=(q, geo_filtered_dict))
         processes.append(p)
         p.start()
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'twitter-sentiment-analysis-f22ce784b0a8.json'
-    try:
-        for path, dirs, files in os.walk('untarred'):
-            q.put((path, dirs, files))
-        q.join()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        q.close()
-        geo_filtered = set(geo_filtered_dict.keys())
-        with open('geo_filtered.pkl', 'wb') as f:
-            pickle.dump(geo_filtered, f)
-        print('done')
-        for p in processes:
-            p.terminate()
-
-
+    for path, dirs, files in os.walk('untarred'):
+        for file_name in files:
+            if not os.path.join(path, file_name) in geo_filtered_dict and file_name.endswith('.json.bz2'):
+                q.put((path, file_name))
+    q.join()
+    geo_filtered = set(geo_filtered_dict.keys())
+    with open('geo_filtered.pkl', 'wb') as f:
+        pickle.dump(geo_filtered, f)
+    print('done, safe to ctrl-c if it does not exit automatically')
+    for p in processes:
+        p.terminate()
